02bfa787ef3f3e8375f3f71d804aadb208c5e668
edmo339 <teggy1@hotmail.com>
2023-02-06 13:10:00 +0100

    sorted bug

--- src/App.js
+++ src/App.js
@@ -1,58 +1,62 @@
-import './App.css'; 
+import logo from './logo.svg';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, InputGroup, FormControl, Button, Row, Card } from 'react-bootstrap';
-import { useState, useEffect } from 'react'
+import { Container, InputGroup, FormControl, Button, Row, Card, Form } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
 
-const CLIENT_ID = "0937518b77fc47a1bdb079643ff9f53e";
-const CLIENT_SECRET = "d5fe8f22936040868f90ce3ca06e75fe";
+const CLIENT_ID = '369be8bfc67a410e843d382d7f3072a8';
+const CLIENT_SECRET = 'b67608b5dbb24a71ab6f7ad54b8e69d5';
+
 
 function App() {
-  const [searchInput, setSearchInput] = useState("");
-  const [accessToken, setAccessToken] = useState("");
-  const [albums, setalbums, ] = useState([]);
+  const [ searchInput, setSearchInput ] = useState("");
+  const [ accessToken, setAccessToken ] = useState("");
+  const [ albums, setAlbums ] = useState([]);
 
-  useEffect(() => {
-    // API Access Token
-    var authParamaters = {
-      method: 'POST',
+  useEffect (() => {
+    //API Access Token
+    var authParameters = {
+      method: 'POST' ,
       headers: {
-        'Content-Type' : 'application/x-www-form-urlencoded'
+        'Content-Type' : 'application/x-www-Form-urlencoded'
       },
       body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
     }
-      fetch('https://accounts.spotify.com/api/token', authParamaters )
-      .then(result => result.json())
-      .then(data => setAccessToken(data.access_token))
+    fetch('https://accounts.spotify.com/api/token', authParameters)
+    .then(result => result.json())
+    .then(data => setAccessToken(data.access_token))
   }, [])
 
   // Search
   async function search() {
     console.log("Search for " + searchInput);
-  
 
-  // Get request using search to get the artist ID
-  var searchParamaters = {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + accessToken
+    // Get request using search to get the artist ID
+    var searchParameters = {
+      method: 'GET', 
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer '+ accessToken
+      }
     }
-  }
-  var artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', searchParamaters )
-  .then(response => response.json())
-  .then(data => { return data.artists.items[0].id})
+    var artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist' , searchParameters)
+    .then(response => response.json())
+    .then(data => { return data.artists.items[0].id })
 
-  console.log("Artist ID is " + artistID)
-  // Get request with artist to grab all the albums for that artist
-  var returnedAlbums = await fetch('https//api.spotify.com/v1/artists/' + artistID + '/albums' + '?include_groups=album&market=US&limit=10', searchParamaters)
+    console.log("Artist ID is " + artistID);
+    // Get request with Artist ID to obtain tracks
+
+    var returnedAlbums = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/albums' + '?include_groups=album&market=US&limit=50', searchParameters)
     .then(response => response.json())
     .then(data => {
       console.log(data);
-      setalbums(data.items);
+      setAlbums(data.items);
     });
-  // Display those albums to user
+    
+    // Display albums
   }
   console.log(albums);
+
   return (
     <div className="App">
       <Container>
@@ -60,9 +64,9 @@ function App() {
           <FormControl
           placeholder='Search For Artist'
           type='input'
-          onKeyPress={event => {
+          onKeyPress={ event => {
             if (event.key == "Enter") {
-              search();
+            search();
             }
           }}
           onChange={event => setSearchInput(event.target.value)}
@@ -70,23 +74,25 @@ function App() {
           <Button onClick={search}>
             Search
           </Button>
-        </InputGroup>
-          <Container>
-            <Row className='mx-2 row row-cols-4'>
-              {albums.map( (album, i) => {
-                console.log(album);
-                return (
-              <Card>
-               <Card.Img src={album.images[0].url} />
-                  <Card.Body>
-                    <Card.Title>{album.name}</Card.Title>
-                  </Card.Body>
-              </Card>
-                )
-              })}
-            
-            </Row>
-          </Container>
+
+       </InputGroup>
+      </Container>
+      <Container>
+        <Row className='mx-2 row row-cols-4'>
+          {albums.map( (album, i) => {
+            console.log(album);
+            return (
+            <Card>
+            <Card.Img src={album.images[0].url} />
+            <Card.Body>
+              <Card.Title>{album.name}</Card.Title>
+            </Card.Body>
+          </Card>
+          )
+          })}
+        
+        </Row>
+        
       </Container>
     </div>
   );
